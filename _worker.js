cf9f159c43cb2a267c2a78ff714baa506bed6636
MaJiD-PaGeS <137340426+MaJiD-PaGeS@users.noreply.github.com>
2024-04-10 00:46:38 +0330

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -4,7 +4,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
-let userID = 'cb1df746-f5dc-4b55-8fac-f42a35af6639';
+let userID = '';
 
 let proxyIP = '';// 小白勿动，该地址并不影响你的网速，这是给CF代理使用的。'cdn.xn--b6gac.eu.org, cdn-all.xn--b6gac.eu.org, workers.cloudflare.cyou'
 
