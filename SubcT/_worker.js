7703e5cc4e4f00a6c23d800b3e5aae875f01a3d4
MaJiD-PaGeS <137340426+MaJiD-PaGeS@users.noreply.github.com>
2024-04-09 22:45:20 +0330

    Update _worker.js

--- SubcT/_worker.js
+++ SubcT/_worker.js
@@ -41,7 +41,7 @@ let proxyhosts = [//本地代理域名池
 let proxyhostsURL = 'https://raw.githubusercontent.com/cmliu/CFcdnVmess2sub/main/proxyhosts';//在线代理域名池URL
 let EndPS = '';//节点名备注内容
 
-let FileName = 'WorkerVless2sub';
+let FileName = 'SubcT';
 let SUBUpdateTime = 6; 
 let total = 99;//PB
 //let timestamp = now;
